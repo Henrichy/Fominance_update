b39880be003bd4f0440a03b8d24972629ad19d9e
David <ejeredavid2001@gmail.com>
2024-03-31 23:19:19 +0100

    Made changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.css"
+import Logo from "./../assets/Used Logo.svg"
+import { Link } from "react-router-dom";
 
 function Head() {
   return (
     <>
-        <div className=" font-bold py-4 bg-blue-600 w-full flex justify-around">
-    <NavLink to="/" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      BUY GIFTCARDS
+    <div></div>
+        <div className=" font-semibold py-4 bg-heroBlue w-full flex justify-around items-center pt-[30px]">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+         <div>
+          <input type="" placeholder="Search here" className=" w-[376px] px-[20px] py-[8px] border-solid border-white bg-heroBlue border-[1px] rounded-md placeholder-white font-light "/>
+         </div>
+    <NavLink to="/AboutUs" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
+      About Us
     </NavLink>
-    <NavLink to="/mostpopularcards" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      MOST POPULAR CARDS
+    <NavLink to="/Giftcards" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
+      Giftcards
     </NavLink>
-    <NavLink to="/bestdeals" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      BEST DEALS
+    <NavLink to="/Community" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
+      Community
     </NavLink>
-    <NavLink to="/allcategories" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      ALL CATEGORIES
+    <NavLink to="/Support" activeClassName="active" className="nav text-white flex justify-center items-center h-full ">
+      Support
     </NavLink>
-    <NavLink to="/about" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      COMPANY
+    <NavLink to="/Account" activeClassName="active" className="nav text-white flex justify-center items-center h-full bg-btnBlue px-[40px] py-[13px]  rounded-full font-semibold hover:bg-white hover:text-btnBlue transition ease-linear duration-[0.2s]">
+      My Account
     </NavLink>
-    <NavLink to="/legal" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      LEGAL
-    </NavLink>
-    <NavLink to="/support" activeClassName="active" className="nav text-white flex justify-center items-center h-full">
-      SUPPORT
-    </NavLink>
+    
   </div>
   </>
 
