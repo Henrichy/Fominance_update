4a462046cfe5fadca106e76bee007a1577335186
David <ejeredavid2001@gmail.com>
2024-04-06 19:07:48 +0100

    went back to a previous commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import HeroImg from "./../assets/heroImg.svg"
 
 function Hero (){
     return(
-        <div className="bg-heroBlue text-center ">
+        <div className="bg-heroBlue text-center mt-[-10px]">
             <h1 className="text-white font-[600] text-[64px] text-center pt-[80px]">Welcome to Fominance!</h1>
-            <h2 className="text-white text-[40px] font-[500] text-center">Your home of all things Giftcard.</h2>
+            <h2 className="text-white text-[40px] font-[500]  text-center">Your home of all things Giftcard.</h2>
             <button className=" border-[1px] border-solid border-btnBlue text-white rounded-full w-[162px] h-[50px] mx-auto mt-7 hover:bg-btnBlue transition ease-linear ">Get Started</button>
             <img src={HeroImg} alt="" className="mx-auto "/>
                 
