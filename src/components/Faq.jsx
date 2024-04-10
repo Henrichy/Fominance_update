0337a11b862066accabd85a2c48ab45f355f41fe
Henrichy <107367296+Henrichy@users.noreply.github.com>
2024-04-10 13:56:41 +0100

    faqs done

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Footer from "./footer";
 import Head from "./Nav";
 import sallyA from "../assets/Saly14.svg"
+import Saly29 from "../assets/Saly29.svg"
+import Saly27 from "../assets/Saly27.png"
+import Saly12 from "../assets/Saly12.svg"
+import Saly45 from "../assets/Saly45.svg"
+
 import icon from "../assets/icon.png"
 
 import './affiliate.css'
 import './blog.css'
 import './Faq.css'
+import Supportcomp from "./Supportcomp";
 
 
 
@@ -16,13 +22,13 @@ function Faq (){
     return (
         <>
         <Head /> 
-       
-        <div className=' theboxfaq flex flex-col h-[750px]'>
-            <div className='relative justify-center flex h-auto w-[100%]'>
-            <img className='absolute w-[100%] top-0 h-[750px]' src={sallyA} alt="" />
-            <h1 className='absolute flex text-gray-700 font-bold text-5xl top-[50px]'>How might we <h2 className="text-orange-300">&nbsp; help you?</h2></h1>
+        <div className="thebigb "></div>
 
-            <div className='inputt'>
+        <div className=" justify-center flex items-center flex-col">
+            <div className="relative flex justify-center items-center">
+        <img className='w-[100%]' src={sallyA} alt="" />
+        <h1 className=' absolute top-[20px] flex text-gray-700 font-bold text-5xl'>How might we <h2 className="text-orange-300">&nbsp; help you?</h2></h1>
+        <div className='inputt'>
         <div className="therelative relative flex items-center bg-white flex rounded-lg items-center justify-around px-4 py-2 gap-2">
             <img className="h-4 w-auto absolute left-4 top-2.2" src={icon} alt="Logo" />
             <input type="text" className="focus:outline-none ml-6 w-full"
@@ -31,9 +37,28 @@ function Faq (){
             <button className="bg-blue-600 h-8 pl-3 pr-3 text-white rounded-lg">Search</button>
           </div>
           </div>
-      
         </div>
-        </div>     
+
+        </div>
+        <div className="conta flex justify-center flex-col items-center">
+            <div className="flex justify-center z-10 mt-[-90px] items-center gap-14 flex-wrap py-10 w-[70%]">
+                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly29} alt="" /><h3 className="font-semibold">Giftcards</h3></div>
+                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly12} alt="" /><h3 className="font-semibold">Accounts and Login</h3></div>
+                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly45} alt="" /><h3 className="font-semibold">Ordering and Payment</h3></div>
+                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly27} alt="" /><h3 className="font-semibold">2FA and Security</h3></div>
+            </div>
+            <h3 className="font-bold asked">Frequently Asked Questions.</h3>
+            <div className=" w-[100%] flex flex-col justify-center items-center gap-4  my-8">
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold"> How can I purchase a gift card?</span><span className="text-blue-600">+</span></div>
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold"> Can i customize my giftcard with personal message?</span><span className="text-blue-600">+</span></div>
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold">  Can I send a gift card to someone else?</span><span className="text-blue-600">+</span></div>
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold">  What brands are available for gift cards?</span><span className="text-blue-600">+</span></div>
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold"> How secure is my personal information when using this app?</span><span className="text-blue-600">+</span></div>
+            <div className="expand px-10 text-left flex items-center justify-between"><span className="font-semibold"> Can i store my unused giftcard on fominance?</span><span className="text-blue-600">+</span></div>
+
+            </div>
+        </div>
+        <Supportcomp />
         <Footer/>
         </>
     );
