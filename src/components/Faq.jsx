1c7d2aba774db02f29944bde6430a41c965be366
Henrichy <107367296+Henrichy@users.noreply.github.com>
2024-04-15 17:54:17 +0100

    faq updated

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,57 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./footer";
 import Head from "./Nav";
-import sallyA from "../assets/Saly14.svg"
-import Saly29 from "../assets/Saly29.svg"
-import Saly27 from "../assets/Saly27.png"
-import Saly12 from "../assets/Saly12.svg"
-import Saly45 from "../assets/Saly45.svg"
+import sallyA from "../assets/Saly14.svg";
+import Saly29 from "../assets/Saly29.svg";
+import Saly27 from "../assets/Saly27.png";
+import Saly12 from "../assets/Saly12.svg";
+import Saly45 from "../assets/Saly45.svg";
 
-import icon from "../assets/icon.png"
+import icon from "../assets/icon.png";
 
-import './affiliate.css'
-import './blog.css'
-import './Faq.css'
+import './affiliate.css';
+import './blog.css';
+import './Faq.css';
 import Supportcomp from "./Supportcomp";
 import FaqSection from "./FaqSection";
-
-
-
-
+import FaqSectionGiftcard from "./FaqSectionGiftcard";
+import FaqSectionAcc from "./FaqSectionAcc";
 
 function Faq (){
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [isGiftcardClicked, setIsGiftcardClicked] = useState(false);
+    const [isAccClicked, setIsAccClicked] = useState(false);
+
+
+    const handleGiftcardClick = () => {
+        setSelectedCategory('giftcard');
+        setIsGiftcardClicked(true);
+        setIsAccClicked(false);
+    };
+    
+    const handleAccClick = () => {
+        setSelectedCategory('acc');
+        setIsAccClicked(true);
+        setIsGiftcardClicked(false);
+
+        
+    };
+
     return (
         <>
         <Head /> 
 
-        <div className=" justify-center w-full flex items-center flex-col">
+        <div className="justify-center w-full flex items-center flex-col">
             <div className="relative flex justify-center items-center">
-        <img className='w-full' src={sallyA} alt="" />
-        <h1 className=' absolute top-[20px] flex text-gray-700 font-bold text-5xl'>How might we <h2 className="text-orange-300">&nbsp; help you?</h2></h1>
-        <div className='inputt'>
-        <div className="therelative relative flex items-center bg-white flex rounded-lg items-center justify-around px-4 py-2 gap-2">
-
-            <img className="h-4 w-auto absolute left-4 top-2.2" src={icon} alt="Logo" />
-            <input type="text" className="focus:outline-none ml-6 w-full"
-              placeholder="Enter your question here"
-            />
-            <button className="bg-blue-600 h-8 pl-3 pr-3 text-white rounded-lg">Search</button>
-          </div>
-          </div>
+                <img className='w-full' src={sallyA} alt="" />
+                <h1 className='absolute top-[20px] flex text-gray-700 font-bold text-5xl'>How might we <h2 className="text-orange-300">&nbsp; help you?</h2></h1>
+                <div className='inputt'>
+                    <div className="therelative relative flex items-center bg-white flex rounded-lg items-center justify-around px-4 py-2 gap-2">
+                        <img className="h-4 w-auto absolute left-4 top-2.2" src={icon} alt="Logo" />
+                        <input type="text" className="focus:outline-none ml-6 w-full" placeholder="Enter your question here" />
+                        <button className="bg-blue-600 h-8 pl-3 pr-3 text-white rounded-lg">Search</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        </div>
         <div className="conta flex justify-center flex-col items-center">
             <div className="flex justify-center z-10 mt-[-90px] items-center gap-14 flex-wrap py-10 w-[70%]">
-                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly29} alt="" /><h3 className="font-semibold">Giftcards</h3></div>
-                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly12} alt="" /><h3 className="font-semibold">Accounts and Login</h3></div>
-                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly45} alt="" /><h3 className="font-semibold">Ordering and Payment</h3></div>
-                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12"><img className="w-[200px] h-[150px]" src={Saly27} alt="" /><h3 className="font-semibold">2FA and Security</h3></div>
+            <div className={`flex pointer flex-col justify-center items-center bg-white shadow-lg rounded-md p-12 ${isGiftcardClicked ? 'bg-blue-100' : ''}`} onClick={handleGiftcardClick}>
+                    <img className="w-[200px] h-[150px]" src={Saly29} alt="" />
+                    <h3 className="font-semibold">Giftcards</h3>
+                </div>
+                <div className={`flex pointer flex-col justify-center items-center bg-white shadow-lg rounded-md p-12 ${isAccClicked ? 'bg-blue-100' : ''}`} onClick={handleAccClick}>
+                    <img className="w-[200px] h-[150px]" src={Saly12} alt="" />
+                    <h3 className="font-semibold">Accounts and Login</h3>
+                </div>
+                <div className="pointer flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12">
+                    <img className="w-[200px] h-[150px]" src={Saly45} alt="" />
+                    <h3 className="font-semibold">Ordering and Payment</h3>
+                </div>
+                <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md p-12">
+                    <img className="w-[200px] h-[150px]" src={Saly27} alt="" />
+                    <h3 className="font-semibold">2FA and Security</h3>
+                </div>
             </div>
             <h3 className="font-bold asked">Frequently Asked Questions.</h3>
-            <div className=" w-[100%] flex flex-col justify-center items-center gap-4  my-8">
-            <FaqSection />
-
+            <div className="w-[100%] flex flex-col justify-center items-center gap-4  my-8">
+                {!selectedCategory && <FaqSection />}
+                {selectedCategory === 'giftcard' ? <FaqSectionGiftcard /> : null}
+                {selectedCategory === 'acc' ? <FaqSectionAcc /> : null}
             </div>
         </div>
         <Supportcomp />
